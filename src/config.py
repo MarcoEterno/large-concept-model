0d660c20a79dad56fa1ac7a13b4caa14bc9c5ad7
marco eterno <Marcolino97do@gmail.com>
2024-07-24 18:58:12 +0200

    implemented top-p rollout policy in the llm.py module

--- src/config.py
+++ src/config.py
@@ -1,4 +1,8 @@
 import torch
+
 DEVICE = 'mps' if torch.backends.mps.is_built() else 'cuda' if torch.cuda.is_available() else 'cpu'
 TOP_K = 5
-TOP_P = 0.05
+HIGHER_THAN_P = 0.05  # tokens with probability less than this will be discarded.
+# if no token is left, the token with the highest probability will be chosen
+
+
