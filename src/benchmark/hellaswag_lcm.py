implemented the correct attention mask in the hellaswag_lcm.py module for lcm evaluation

src/benchmark/hellaswag_lcm.py
@@ -182,18 +182,21 @@ def evaluate(model_type, device, one_example_every_n = 1):
 
 @torch.no_grad()
 def evaluate_lcm(model_checkpoint, n_tokens_per_concept,  device, one_example_every_n = 1):
-
+    # TODO: check that attention mask for BERT is right
+    # TODO: check that the attention mask for LCM core is right
     torch.set_float32_matmul_precision('high') # use tf32
 
-    checkpoint = torch.load(model_checkpoint, map_location=torch.device('cpu'))
+    checkpoint = torch.load(model_checkpoint, map_location=torch.device('cpu')) # TODO: speed up
     state_dict = checkpoint["model"]
     model = CoreLCM(LCMConfig)
     model.load_state_dict(state_dict, strict=False)
     model.to(device)
+    model.eval()
+    print(model)
+    # model = torch.compile(model) # optionally torch compile the model and the encoder, helpful for bigger models
 
     encoder = Encoder(n_tokens_per_concept=n_tokens_per_concept).to(device)
-    # model = torch.compile(model) # optionally torch compile the model
-
+    encoder.eval()
     num_correct_norm = 0
     num_correct = 0
     iterations = 0
@@ -212,16 +215,26 @@ def evaluate_lcm(model_checkpoint, n_tokens_per_concept,  device, one_example_ev
         context_concepts = encoder.encode_tokens(torch.Tensor(data["ctx_tokens"]).to(device))
 
         # Pad ending tokens to the maximum length
-        padding_token = [0] # check if this is true for BERT and make this into the encoder
         max_len = max(len(ending) for ending in data["ending_tokens"])
-        padded_endings = [ending + padding_token * (max_len - len(ending)) for ending in data["ending_tokens"]]
+        padded_endings = [ending + [encoder.tokenizer.pad_token_id] * (max_len - len(ending)) for ending in data["ending_tokens"]]
         ending_concepts = encoder.encode_tokens(torch.Tensor(padded_endings).to(device))
 
         # concatenate the context and the ending concepts by copying the context concepts 4 times and pasting them before ending concepts
         concepts_real = torch.cat([context_concepts.repeat(4, 1, 1), ending_concepts], dim=1)
 
         # create the mask for the concepts
-        concept_mask = torch.cat([torch.zeros_like(context_concepts).repeat(4, 1, 1), torch.ones_like(ending_concepts)], dim=1)
+        max_ending_concept_len = ending_concepts.size(1)
+        ending_concept_mask = torch.zeros(4, max_ending_concept_len, dtype=torch.float32, device=device)
+        for i in range(4):
+            concept_position = -1
+            for position, ending_token in enumerate(data["ending_tokens"][i]):
+                if position % n_tokens_per_concept != 0:
+                    continue
+                concept_position += 1
+                if ending_token == encoder.tokenizer.pad_token_id:
+                    break
+                ending_concept_mask[i, concept_position] = 1
+        concept_mask = torch.cat([torch.zeros(context_concepts.shape[:-1],dtype=torch.float32, device=device).repeat(4, 1), ending_concept_mask], dim=1)
 
         # get the forecasted concepts
         concept_forecast, loss = model(concepts_real)
@@ -236,12 +249,12 @@ def evaluate_lcm(model_checkpoint, n_tokens_per_concept,  device, one_example_ev
 
         # calculate loss
         #shift_losses = F.cross_entropy(flat_shift_logits, flat_shift_concepts, reduction='none')
-        shift_losses = 1 - F.cosine_similarity(shift_concepts_forecast, shift_concepts_real, dim=-1)# check what is the mean for, should be mean on the concepts of the single sentence
+        shift_losses = - F.cosine_similarity(shift_concepts_forecast, shift_concepts_real, dim=-1)# check what is the mean for, should be mean on the concepts of the single sentence
         # shift_losses = shift_losses.view(concepts_real.size(0), -1)
 
         # now get the average loss just for the completion region (where mask == 1), in each row
         # we need to create a new mask for it, that carefully separates the input from the continuation
-        shift_concept_mask = (concept_mask[..., 1:, :]).mean(-1).contiguous() # we must shift mask, so we start at the last prompt token
+        shift_concept_mask = (concept_mask[..., :, 1:]).contiguous() # we must shift mask, so we start at the last prompt token
         masked_shift_losses = shift_losses * shift_concept_mask
 
         # sum and divide by the number of 1s in the mask
@@ -250,7 +263,7 @@ def evaluate_lcm(model_checkpoint, n_tokens_per_concept,  device, one_example_ev
 
         # now we have a loss for each of the 4 completions
         # the one with the lowest loss should be the most likely
-        pred = sum_loss.argmin().item()
+        pred = avg_loss.argmin().item()
         pred_norm = avg_loss.argmin().item()
 
         # accumulate stats