5557a353dd22efd99b6932851e3384ab0dd71058
MarcoEterno <marcolino97do@gmail.com>
2024-09-27 16:23:33 +0000

    minor changes to better train gpt in the train_gpt.py module

--- src/train/train_gpt.py
+++ src/train/train_gpt.py
@@ -155,7 +155,7 @@ for step in range(max_steps):
                 torch.save(checkpoint, checkpoint_path)
 
     # once in a while evaluate hellaswag
-    if (step % 1 == 0 or last_step) and (not use_compile):
+    if step > 1 and (step % 100 == 0 or last_step) and (not use_compile):
         num_correct_norm = 0
         num_total = 0
         for i, example in enumerate(iterate_examples("val")):
@@ -188,7 +188,7 @@ for step in range(max_steps):
                 f.write(f"{step} hella {acc_norm:.4f}\n")
 
     # once in a while generate from the model (except step 0, which is noise)
-    if ((step > 0 and step % 250 == 0) or last_step) and (not use_compile):
+    if ((step > 1 and step % 250 == 0) or last_step) and (not use_compile):
         model.eval()
         num_return_sequences = 4
         max_length = 32
